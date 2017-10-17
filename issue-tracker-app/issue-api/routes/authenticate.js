f1929f585bd6d6ecabe066c060c7894e54ddd568
Michael Pollard <msp4k@mtmail.mtsu.edu>
2017-10-17 11:46:22 -0500

    added a connection string to delete from db

--- issue-tracker-app/issue-api/routes/authenticate.js
+++ issue-tracker-app/issue-api/routes/authenticate.js
@@ -1,7 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var Sequelize = require('sequelize')
+//var conn = require('./public/javascripts/conn')(dbConnect);
+
+
 
 module.exports = function(passport) {
+
+  /* establish a connection with the database */
+  const conn = new Sequelize('issueTrackerDB', 'ApolloAdmin', 'IssueTracker2017', {
+      host: 'mtsu-4700-2017.database.windows.net',
+      dialect: 'mssql',
+      driver: 'tedious',
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      },
+      port: 1433,
+      dialectOptions: {
+        encrypt: true
+      }
+  });
+  // authenticate the connection
+  conn
+      .authenticate()
+      .then(() => {
+          console.log('Connection has been established successfully - auth.');
+      })
+      .catch(err => {
+          console.error('Unable to connect to the database:', err);
+  });
+
+  const User = conn.import('../models/IssueTracker_Users.js');
 
   //send a successful login state back to angular
   router.get('/success', function(req, res) {
@@ -33,5 +64,17 @@ module.exports = function(passport) {
     req.redirect('/');
   });
 
+  //delete entry by id (for getting rid of multiple entries)
+  router.delete('/drop/:id', (req, res, next) => {
+      console.log(req.params.id);
+      User.destroy({
+        where: {
+          UserId: req.params.id,
+        }
+      })
+      .then(result => {
+        console.log('Deleted user: $result.UserId - $result.UserName');
+      });
+  });
   return router;
 };
